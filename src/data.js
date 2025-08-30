c95bef53e471b9d67cf8dd6c0acba5d8c671bc3e
praveenkumar663 <praveen060600@gmail.com>
2025-08-30 21:08:16 +0530

    skill

--- src/data.js
+++ src/data.js
@@ -1,5 +1,12 @@
-// experince and certfication for about section
 
+import { FaCode, FaHtml5, FaCss3Alt, FaReact, FaBootstrap, FaNodeJs, FaDatabase, FaTools, FaJava, FaPython } from "react-icons/fa";
+import { SiJavascript, SiMongodb, SiExpress, SiMysql, SiPostman, SiGit, SiFigma, SiC, SiCplusplus } from "react-icons/si";
+import { BiLogoVisualStudio } from "react-icons/bi";
+
+
+
+
+// experince and certfication for about section
 export const Experience = [
   {
     subtitle: "Software Developer Internship",
@@ -23,3 +30,56 @@ export const Certificates = [
   { certify: "Advanced Diploma in Python Programming [ADPP]" },
   { certify: "Postgraduate In Diploma Computer Application [PGDCA]" }
 ];
+
+// skills
+//  ================= frontend =================
+export const frontend = {
+  titleIcon: FaCode,
+  title: "Frontend Technologies",
+  skills: [
+    { name: "HTML", icon: FaHtml5, color: "#ff5526ff" },  // orange-red
+    { name: "CSS", icon: FaCss3Alt, color: "#0494faff" }, // blue
+    { name: "JavaScript", icon: SiJavascript, color: "#F7DF1E" }, // yellow
+    { name: "React", icon: FaReact, color: "#0ef" },  // cyan
+    { name: "Bootstrap", icon: FaBootstrap, color: "#bb07f1ff" } // purple
+  ]
+};
+
+
+// ================= BACKEND =================
+export const backend = {
+  titleIcon: FaCode,
+  title: "Backend & Programming",
+  skills: [
+    { name: "Node.js", icon: FaNodeJs, color: "#0abe04ff" },
+    { name: "Express.js", icon: SiExpress, color: "#fff" },
+    { name: "Python", icon: FaPython, color: "#2da0ffff" },
+    { name: "Java", icon: FaJava, color: "#fde400ff" },
+    { name: "C", icon: SiC, color: "#b6cce6ff" },
+    { name: "C++", icon: SiCplusplus, color: "#04fafaff" }
+  ]
+};
+
+
+// DATABASE
+export const Database = {
+  titleIcon: FaDatabase,
+  title: "Database",
+  skills: [
+    { name: "MongoDB", icon: SiMongodb, color: "#01af04ff" },
+    { name: "MySQL", icon: SiMysql, color: "#fcc30bff" },   
+  ]
+};
+
+// ================= TOOLS =================
+export const tools = {
+  titleIcon: FaTools,
+  title: "Tools & Others",
+  skills: [
+    { name: "Postman", icon: SiPostman, color: "#FF6C37" },
+    { name: "Git", icon: SiGit, color: "#F1502F" },
+    { name: "Figma", icon: SiFigma, color: "#F24E1E" },
+    { name: "VS Code", icon: BiLogoVisualStudio, color: "#007ACC" }
+  ]
+};
+
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import {frontend,backend,Database, tools} from '../data.js'
+import '../styles/Skill.css'
+import { FaBullseye,FaBookOpen} from "react-icons/fa6";
 
 const Skills = () => {
   return (
@@ -6,12 +8,154 @@ const Skills = () => {
     <div className="container-fluid skills">
       {/* title */}
         <div className="row">
-          <h2 className='text-center  fw-bold mb-3 skill-title'>My <span>Skills</span> </h2>
+          <h2 className='text-center  fw-bold mb-3 skill-title'>My <span className="title-color">Skills</span> </h2>
         </div>
         {/* skill content */}
-          <div className="row skill-content">
+          <div className="row skill-content my-3">
             <p>These are my core skills, along with the technologies I am currently learning and the areas I plan to explore in the future. </p>
           </div>
+
+      {/* skills  */}
+
+        <div className="row skill-details">
+        {/* frontend */}
+          <div className="col-sm-12 col-md-6 col-lg-4">
+            <div className="card">
+              <div className="card-title-logo">
+                <span className="card-title-icon me-2"><frontend.titleIcon/></span>
+                <span className="card-title">{frontend.title}</span>
+              </div>
+                 <div className="card-body">
+                   <ul className="list-group list-group-flush ">
+
+                    {frontend.skills.map((skill,index)=>(
+                       <li className="list-group-item" style={{color:skill.color}} key={index}>
+                      <skill.icon/>
+                      </li>
+                    ))}
+                   </ul>
+                   <ul className="icon-name mt-4"> 
+                    {frontend.skills.map((skill,index)=>(
+                      <li key={index}>{skill.name}</li>
+                    
+                    ))}
+                   </ul>
+                 </div>
+            </div>
+          </div>
+                {/* backend */}
+          <div className="col-sm-12 col-md-6 col-lg-4">
+            <div className="card">
+              <div className="card-title-logo">
+                <span className="card-title-icon me-2"><backend.titleIcon/></span>
+                <span className="card-title">{backend.title}</span>
+              </div>
+                 <div className="card-body">
+                   <ul className="list-group list-group-flush ">
+
+                    {backend.skills.map((skill,index)=>(
+                       <li className="list-group-item" style={{color:skill.color}} key={index}>
+                      <skill.icon/>
+                      </li>
+                    ))}
+                   </ul>
+                   <ul className="icon-name mt-4"> 
+                    {backend.skills.map((skill,index)=>(
+                      <li key={index}>{skill.name}</li>
+                    
+                    ))}
+                   </ul>
+                 </div>
+            </div>
+          </div>
+
+                {/* DATABASE*/}
+          <div className="col-sm-12 col-md-6 col-lg-4">
+            <div className="card">
+              <div className="card-title-logo">
+                <span className="card-title-icon me-2"><Database.titleIcon/></span>
+                <span className="card-title">{Database.title}</span>
+              </div>
+                 <div className="card-body">
+                   <ul className="list-group list-group-flush ">
+
+                    {Database.skills.map((skill,index)=>(
+                       <li className="list-group-item" style={{color:skill.color}} key={index}>
+                      <skill.icon/>
+                      </li>
+                    ))}
+                   </ul>
+                   <ul className="icon-name mt-4"> 
+                    {Database.skills.map((skill,index)=>(
+                      <li key={index}>{skill.name}</li>
+                    
+                    ))}
+                   </ul>
+                 </div>
+            </div>
+          </div>
+
+                {/* Tools*/}
+          <div className="col-sm-12 col-md-6 col-lg-4">
+            <div className="card">
+              <div className="card-title-logo">
+                <span className="card-title-icon me-2"><tools.titleIcon/></span>
+                <span className="card-title">{tools.title}</span>
+              </div>
+                 <div className="card-body">
+                   <ul className="list-group list-group-flush ">
+
+                    {tools.skills.map((skill,index)=>(
+                       <li className="list-group-item" style={{color:skill.color}} key={index}>
+                      <skill.icon/>
+                      </li>
+                    ))}
+                   </ul>
+                   <ul className="icon-name mt-4"> 
+                    {tools.skills.map((skill,index)=>(
+                      <li key={index}>{skill.name}</li>
+                    
+                    ))}
+                   </ul>
+                 </div>
+            </div>
+          </div>
+
+               {/* learning */}
+               <div className="row learning">
+
+                <div className="col-sm-12 col-md-6 col-lg-6">
+                  <div className="current-learning mb-4">
+                    <span className="icon "><FaBookOpen /></span>
+                    <span className="title">Currently Learning</span>
+                    <ul>
+                      <li>Data Structures & Algorithms (Java)</li>
+                      <li>Advanced React Patterns and Optimization</li>
+                      <li>Vue.js(expanding frontend expertise)</li>
+                      <li>Rest APIs & Authentication</li>
+                    </ul>
+                  </div>
+                </div>
+
+                <div className="col-sm-12 col-md-6 col-lg-6">
+                  <div className="future-goal">
+                    <span className="icon "><FaBullseye /></span>
+                    <span className="title">Future Goals</span>
+                    <ul>
+                      <li>Cloud technologies (AWS, Azure)</li>
+                      <li>DevOps and CI/CD pipelines</li>
+                      <li>Mobile development with React Native</li>
+                      <li>AI/Ml</li>
+                    </ul>
+                  </div>
+                </div>
+
+               </div>
+
+        </div>
+
+
+
 
     </div>
 
