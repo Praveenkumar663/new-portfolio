48cccd3089a6ecb4a9608243698fe7f9126709bb
praveenkumar663 <praveen060600@gmail.com>
2025-09-08 20:00:50 +0530

    scrolltotop updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Foooter from './components/Foooter'
 import Navbar from './components/Navbar'
+import { ScrollToTop } from './components/ScrollToTop'
 import AllRoutes from './Routes/AllRoutes'
 
 function App() {
 
   return (
     <>
+      <ScrollToTop/>
       <Navbar />
       <AllRoutes/>
       <Foooter/>
+      
     </>
   )
 }
