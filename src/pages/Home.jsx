9654475e2404be06d8fa6fa1ff6bf7590e34d755
Praveenkumar663 <praveen060600@gmail.com>
2026-07-25 20:51:16 +0530

    home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import "../styles/Home.css";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import profile from "../assets/profile.jpeg";
 import resume from "../../public/praveenkumar-resume.pdf";
+import "../styles/home.css";
 
 const Home = () => {
   const roles = ["Full-Stack Developer", "Freelancer"];
